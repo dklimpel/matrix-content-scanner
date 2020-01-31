03bb12a88fad0fc22f21c7f438944ebc337f913b
Dirk Klimpel <5740567+dklimpel@users.noreply.github.com>
2020-01-31 17:31:07 +0100

    Update example-c-icap-script.sh

--- example-c-icap-script.sh
+++ example-c-icap-script.sh
@@ -1,3 +1,8 @@
+# This is an example script to run when a file is scanned. 
+# - ${1} will be the path to the file to scan.
+# - The exit code should be 0 for files that are deemed safe/clean.
+# - Anything sent to stdout will be included in the `info` section of the response.
+
 # Run the c-icap-client, sending file at $1
 #
 # This sends file at $1 over ICAP, with -v enabled so that we print
